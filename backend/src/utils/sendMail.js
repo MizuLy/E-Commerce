1a26daf5f26320b5873d52d26e1e73ba25a2c390
Sengly Sea <optionalsa@gmail.com>
2026-06-29 11:25:11 +0700

    render so ahh cant even use nodemailer to send otp

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -1,8 +1,8 @@
-const resend = require("../config/resend");
+const transporter = require("../config/otp");
 
 const sendOtp = async (email, otp) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  await transporter.sendMail({
+    from: `"Mizu Co.Ltd" <${process.env.SMTP_USER}>`,
     to: email,
     subject: "Your OTP Code",
     text: `Your OTP code is: ${otp}. Expires in 5 minutes.`,
@@ -10,11 +10,11 @@ const sendOtp = async (email, otp) => {
 };
 
 const sendSuccess = async (email) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  await transporter.sendMail({
+    from: `"Mizu Co.Ltd" <${process.env.SMTP_USER}>`,
     to: email,
     subject: "Register success",
-    text: "You can now log into your account",
+    text: `You can now log into your account`,
   });
 };
 
